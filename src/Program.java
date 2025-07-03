3aea176a00703bc278b76ceb08fcfb88fd76a7d3
Zachnad0 <zjhobbs@gmail.com>
2025-07-03 15:09:57 +1200

    first commit

---
+++ src/Program.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Program
+{
+    /** Entrypoint of server application. No GUI. */
+    public static void main(String[] args)
+    {
+        System.out.println("Server app init success");
+    }
+}
---
+++ src/tests/ExampleTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.Test;
+import org.junit.jupiter.api.*;
+
+public class ExampleTest
+{
+
+    @Test
+    public void testTest()
+    {
+        Assertions.assertEquals(true, true);
+    }
+}
