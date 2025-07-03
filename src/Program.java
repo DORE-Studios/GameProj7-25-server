38dab6121aa8c2702bc0cd49f3ef00dd68675ecb
Zachnad0 <zjhobbs@gmail.com>
2025-07-03 16:52:04 +1200

    ADDED prototype data listening.

--- src/Program.java
+++ src/Program.java
@@ -1,10 +1,55 @@
 import java.util.*;
+import java.io.IOException;
+import java.net.*;
 
-public class Program
+public final class Program
 {
+    private static final int serverPort = 8051;
+
+    private static ServerSocket serverSocket = null;
+    private static boolean isGameRunning = true;
+
     /** Entrypoint of server application. No GUI. */
     public static void main(String[] args)
     {
+        // Init the server
+        try
+        {
+            serverSocket = new ServerSocket(serverPort);
+        }
+        catch (IOException e)
+        {
+            System.out.println("Socket bootup fail");
+            return;
+        }
+
         System.out.println("Server app init success");
+
+        while (isGameRunning)
+        {
+            Socket socket;
+            try
+            {
+                socket = serverSocket.accept();
+                System.out.println("Received connection");
+                onRequestReceived(socket);
+                socket.close();
+            }
+            catch (Exception e)
+            {
+                // If failed, just skip for now
+                continue;
+            }
+        }
+
+        System.out.println("Server ended");
+    }
+
+    private static void onRequestReceived(Socket socket) throws IOException
+    {
+        Objects.requireNonNull(socket);
+        byte[] receivedData = socket.getInputStream().readAllBytes();
+
+        System.out.println(new String(receivedData));
     }
 }
